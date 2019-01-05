861070dafb82b9ce96efff980cb2be8865d2bfa5
Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
2019-01-04 22:48:28 -0800

    POS: example script to list images available on a server
    Signed-off-by: Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>

---
+++ examples/test_pos_list_images.py
@@ -0,0 +1,48 @@
+#! /usr/bin/python2
+#
+# Copyright (c) 2018 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# pylint: disable = missing-docstring
+
+import os
+import subprocess
+
+import tcfl.tc
+import tcfl.tl
+import tcfl.pos
+
+
+@tcfl.tc.interconnect("pos_rsync_server", mode = "all")
+class _(tcfl.tc.tc_c):
+    """
+    List images available in a POS rsync server
+    """
+    def eval(self, ic):
+        ic.power.on()
+        port = ic.tunnel.add(873, ic.kws['ipv4_addr'])	# rsync's
+        rsync_host = ic.rtb.parsed_url.hostname
+        rsync_port = port
+        output = subprocess.check_output(
+            [ 'rsync', '--port', str(rsync_port), rsync_host + '::images/' ],
+            close_fds = True, stderr = subprocess.PIPE)
+        # output looks like:
+        #
+        # drwxrwxr-x          4,096 2018/10/19 00:41:04 .
+        # drwxr-xr-x          4,096 2018/10/11 06:24:44 clear:live:25550
+        # dr-xr-xr-x          4,096 2018/04/24 23:10:02 fedora:cloud-base-x86-64:28
+        # drwxr-xr-x          4,096 2018/10/11 20:52:34 rtk::114
+        # ...
+        #
+        # so we parse for 5 fields, take last
+        imagel = tcfl.pos.image_list_from_rsync_output(output)
+        for image in imagel:
+            print ic.fullid, ":".join(image)
+
+        image = os.environ.get("IMAGE", None)
+        if image:
+            d = {}
+            for _image in imagel:
+                d[_image] = _image
+            print image, tcfl.pos._seed_match(d, image)
