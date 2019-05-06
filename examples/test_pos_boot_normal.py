46d4bddaf460873195e480bb01f6dde0932c9d75
Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
2019-05-06 12:13:07 -0700

    examples: script to boot a target to the provisioned OS
    Signed-off-by: Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>

---
+++ examples/test_pos_boot_normal.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+
+import tcfl.tc
+import tcfl.tl
+import tcfl.pos
+
+@tcfl.tc.interconnect("ipv4_addr")
+@tcfl.tc.target('pos_capable')
+class _test(tcfl.tc.tc_c):
+    """
+    Boot a target to the provisioned OS (not Provisioning OS)
+    """
+
+    def eval(self, ic, target):
+        ic.power.on()
+        target.pos.boot_normal()
+
+    def teardown(self):
+        tcfl.tl.console_dump_on_failure(self)
