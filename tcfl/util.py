9861e1bda7d4953b38bb1c3204ac545549955505
Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
2019-10-23 12:43:33 -0700

    tcfl.util: healtchcheck exercises new power interface
    Signed-off-by: Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>

--- tcfl/util.py
+++ tcfl/util.py
@@ -9,9 +9,9 @@ import logging
 
 import commonl
 import tcfl
+from . import msgid_c
 
-
-def healthcheck_power(rtb, rt):
+def healthcheck_power_old(rtb, rt):
     print "Powering off"
     rtb.rest_tb_target_power_off(rt)
     print "Powered off"
@@ -36,6 +36,64 @@ def healthcheck_power(rtb, rt):
 
     print "power test passed"
 
+def healthcheck_power(_rtb, rt):
+    with msgid_c("cmdline"):
+        class args_c(object):
+            ticket = ''
+
+        target = tcfl.tc.target_c.create_from_cmdline_args(args_c(), rt['id'])
+
+        print "Powering off"
+        target.power.off()
+        print "Powered off"
+
+        print "Querying power status"
+        power = target.power.get()
+        if power != False:
+            msg = "Power should be False, reported %s" % power
+            raise Exception(msg)
+        print "Power is reported correctly as %s" % power
+
+        print "Powering on"
+        target.power.on()
+        print "Powered on"
+
+        print "Querying power status"
+        power = target.power.get()
+        if power != True:
+            msg = "Power should be True, reported %s" % power
+            raise Exception(msg)
+        print "Power is reported correctly as %s" % power
+
+        print "Power cycling"
+        target.power.cycle()
+        print "Power cycled"
+
+        print "Power conmponents: listing"
+        components = target.power.list()
+        print "Power components: listed %s" \
+            % " ".join("%s:%s" % (k, v) for k, v in components)
+
+        print "Querying power status"
+        power = target.power.get()
+        if power != True:
+            msg = "Power should be True, reported %s" % power
+            raise Exception(msg)
+        print "Power is reported correctly as %s" % power
+
+        print "Powering off"
+        target.power.off()
+        print "Powered off"
+
+        print "Querying power status"
+        power = target.power.get()
+        if power != False:
+            msg = "Power should be False, reported %s" % power
+            raise Exception(msg)
+        print "Power is reported correctly as %s" % power
+
+        print "power test passed"
+
 
 def healthcheck(args):
     rtb, rt = tcfl.ttb_client._rest_target_find_by_id(args.target)
@@ -44,7 +102,10 @@ def healthcheck(args):
     rtb.rest_tb_target_acquire(rt)
     print "Acquired"
     try:
-        healthcheck_power(rtb, rt)
+        if 'power' in rt['interfaces']:
+            healthcheck_power(rtb, rt)
+        elif 'tt_power_control_mixin' in rt['interfaces']:
+            healthcheck_power_old(rtb, rt)
     finally:
         print "Releasing"
         rtb.rest_tb_target_release(rt)
@@ -53,7 +114,7 @@ def healthcheck(args):
 
 def argp_setup(arg_subparsers):
     ap = arg_subparsers.add_parser("healthcheck",
-                                   help = "List testcases")
+                                   help = "Do a very basic health check")
     commonl.cmdline_log_options(ap)
     ap.set_defaults(level = logging.ERROR)
     ap.add_argument("target", metavar = "TARGET", action = "store",
