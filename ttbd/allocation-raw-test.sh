b34ceb1f45791d7c81e18d62a47a56058215b101
Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
2020-03-11 12:19:37 -0700

    WIP: quick test script can go to any server
    Signed-off-by: Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>

--- ttbd/allocation-raw-test.sh
+++ ttbd/allocation-raw-test.sh
@@ -1,5 +1,6 @@
 #! /bin/sh -xe
-prefix=https://localhost:5001/ttb-v1
+server=${SERVER:-localhost:5002}
+prefix=https://$server/ttb-v1
 
 curl -c cookies.txt -k -X PUT $prefix/login \
      -d password=none -d username=local
@@ -28,7 +29,7 @@ curl -b cookies.txt -k -X PUT $prefix/allocation  \
     | python -m json.tool | tee out
 allocationid=$(python -c "import sys,json; print json.loads(sys.stdin.read())['allocationid']" < out)
 # good: delete it
-curl -b cookies.txt -k -X DELETE https://localhost:5001/ttb-v1/allocation/$allocationid
+curl -b cookies.txt -k -X DELETE https://$server/ttb-v1/allocation/$allocationid
 
 # good: create for another user
 curl -b cookies.txt -k -X PUT $prefix/allocation \
@@ -37,16 +38,16 @@ curl -b cookies.txt -k -X PUT $prefix/allocation \
      -d 'groups={"dd1":["qu-90a", "nwa", "qu-91a"],"dd2":["nwa", "qu-91a", "qu-92a"]}' \
     | python -m json.tool | tee out
 allocationid=$(python -c "import sys,json; print json.loads(sys.stdin.read())['allocationid']" < out)
-curl -b cookies.txt -k -X GET https://localhost:5001/ttb-v1/allocation/$allocationid > out
+curl -b cookies.txt -k -X GET https://$server/ttb-v1/allocation/$allocationid > out
 python -m json.tool < out
 
 # add guests
-curl -b cookies.txt -k -X PATCH https://localhost:5001/ttb-v1/allocation/$allocationid/guest1
-curl -b cookies.txt -k -X PATCH https://localhost:5001/ttb-v1/allocation/$allocationid/guest2
-curl -b cookies.txt -k -X PATCH https://localhost:5001/ttb-v1/allocation/$allocationid/guest3
-curl -b cookies.txt -k -X PATCH https://localhost:5001/ttb-v1/allocation/$allocationid/guest4
+curl -b cookies.txt -k -X PATCH https://$server/ttb-v1/allocation/$allocationid/guest1
+curl -b cookies.txt -k -X PATCH https://$server/ttb-v1/allocation/$allocationid/guest2
+curl -b cookies.txt -k -X PATCH https://$server/ttb-v1/allocation/$allocationid/guest3
+curl -b cookies.txt -k -X PATCH https://$server/ttb-v1/allocation/$allocationid/guest4
 # list guests
-curl -b cookies.txt -k -X GET https://localhost:5001/ttb-v1/allocation/$allocationid \
+curl -b cookies.txt -k -X GET https://$server/ttb-v1/allocation/$allocationid \
     | python -c "import sys,json; print ' '.join(sorted(json.loads(sys.stdin.read())['guests']))" > guests
 if [ "$(< guests)" != "guest1 guest2 guest3 guest4" ]; then
     echo "ERROR: not four guests as expected" 1>&2
@@ -54,9 +55,9 @@ if [ "$(< guests)" != "guest1 guest2 guest3 guest4" ]; then
 fi
 
 # delete one
-curl -b cookies.txt -k -X DELETE https://localhost:5001/ttb-v1/allocation/$allocationid/guest4
+curl -b cookies.txt -k -X DELETE https://$server/ttb-v1/allocation/$allocationid/guest4
 # list guests
-curl -b cookies.txt -k -X GET https://localhost:5001/ttb-v1/allocation/$allocationid \
+curl -b cookies.txt -k -X GET https://$server/ttb-v1/allocation/$allocationid \
     | python -c "import sys,json; print ' '.join(sorted(json.loads(sys.stdin.read())['guests']))" > guests
 if grep guest4 guests; then
     echo "ERROR: guest4 was removed, shan't be there" 1>&2
@@ -68,11 +69,11 @@ if [ "$(< guests)" != "guest1 guest2 guest3" ]; then
 fi
 
 # deleting the same multiple times has no effect
-curl -b cookies.txt -k -X DELETE https://localhost:5001/ttb-v1/allocation/$allocationid/guest4
-curl -b cookies.txt -k -X DELETE https://localhost:5001/ttb-v1/allocation/$allocationid/guest4
+curl -b cookies.txt -k -X DELETE https://$server/ttb-v1/allocation/$allocationid/guest4
+curl -b cookies.txt -k -X DELETE https://$server/ttb-v1/allocation/$allocationid/guest4
 # list guests
-curl -b cookies.txt -k -X GET https://localhost:5001/ttb-v1/allocation/$allocationid | python -m json.tool
-curl -b cookies.txt -k -X GET https://localhost:5001/ttb-v1/allocation/$allocationid \
+curl -b cookies.txt -k -X GET https://$server/ttb-v1/allocation/$allocationid | python -m json.tool
+curl -b cookies.txt -k -X GET https://$server/ttb-v1/allocation/$allocationid \
     | python -c "import sys,json; print ' '.join(sorted(json.loads(sys.stdin.read())['guests']))" > guests
 if [ "$(< guests)" != "guest1 guest2 guest3" ]; then
     echo "ERROR: not just three guests as expected" 1>&2
