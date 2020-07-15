e46caf06ea97ce305131fa2581cbcf26990ab232
Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
2020-07-15 10:29:50 -0700

    ttbd.user_control: fix error path for invalid user
    Signed-off-by: Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>

--- ttbd/ttbl/user_control.py
+++ ttbd/ttbl/user_control.py
@@ -80,7 +80,7 @@ class User(object):
             commonl.makedirs_p(path)
         try:
             self.fsdb = ttbl.fsdb_symlink_c(path)
-        except AssertionError:	# FIXME: replace with invalid_e
+        except ( AssertionError, ttbl.fsdb_c.exception ) as e:
             if fail_if_new:
                 raise self.user_not_existant_e("%s: no such user" % userid)
         self.fsdb.set('userid', userid)
