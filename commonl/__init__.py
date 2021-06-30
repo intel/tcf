e29000028e2a3b3e4fdbe7c40dab782eadc66a6c
Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
2021-06-29 22:23:06 -0700

    commonl.process_started(): add path info to "no PID yet" message
    Signed-off-by: Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>

--- commonl/__init__.py
+++ commonl/__init__.py
@@ -868,8 +868,8 @@ def process_started(pidfile, path,
         time.sleep(poll_period)		# Give it .1s to come up
         pid = process_alive(pidfile, path)
         if pid == None:
-            log.debug("%s: no PID yet (+%.2f/%ss), re-checking", tag,
-                      t - t0, timeout)
+            log.debug("%s: no %s PID yet (+%.2f/%ss), re-checking",
+                      tag, path, t - t0, timeout)
             continue
         # PID found, if there is a verification function, let's run it
         break
