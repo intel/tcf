e625ae34ff796d0e254e18d299222da546c503ee
Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
2021-08-11 11:26:24 -0700

    example/ptest_runner: fix licensing
    Signed-off-by: Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>

--- examples/test_ptest_runner.py
+++ examples/test_ptest_runner.py
@@ -2,7 +2,7 @@
 #
 # Copyright (c) 2019 Intel Corporation
 #
-# SPDX-License-Identifier: Proprietary
+# SPDX-License-Identifier: Apache-2.0
 #
 # pylint: disable = missing-docstring
 """.. _example_ptest_runner:
