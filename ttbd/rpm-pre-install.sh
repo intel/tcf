eb4d1f8eeb7b52a877283370bc5bf634a9ac6e11
Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
2022-04-15 15:02:00 -0700

    ttbd/rpm-pre-install: support pre-existing ttbd group

--- ttbd/rpm-pre-install.sh
+++ ttbd/rpm-pre-install.sh
@@ -4,4 +4,7 @@ if [ x$(stat --format %U /var/run/ttbd-production 2> /dev/null) != xttbd ]; then
     # be re-created with the new user by systemd
     rm -rf /var/run/ttbd-* /var/cache/ttbd-*
 fi
-id ttbd >& /dev/null || useradd -r -d /var/lib/ttbd -c "TCF TTBD Server" -MU ttbd > /dev/null
+# if the group ttbd doesn't exist already, create it
+getent group ttbd || groupadd ttbd
+# if the user ttbd doesn't exist, create it adding it to the group
+id ttbd >& /dev/null || useradd -r -d /var/lib/ttbd -c "TCF TTBD Server" -M -g ttbd ttbd > /dev/null
