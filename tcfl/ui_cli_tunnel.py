3881ae2556cfb0c512e77f483ea0cc4d7b61c047
Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
2023-05-04 08:04:06 -0700

    tcf/tunnel*: split command line implementation to tcfl.ui_cli_tunnel
    This creates the set of commands tunnel-{add,rm,ls}2, meant to replace
    the old implementation in tcfl.target_ext_tunnel that shall just be
    the API extension for tunnels.
    
    This new code also uses the new enumeration API, so it is way faster
    to initialize.

--- tcfl/ui_cli_tunnel.py
+++ tcfl/ui_cli_tunnel.py
@@ -5,33 +5,29 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 """
-Command line interface UI to to manage target power
----------------------------------------------------
+Command line interface UI to to manage tunnels to targets
+---------------------------------------------------------
 
 The following UI commands are available (use ``--help`` on each to
 learn more about them):
 
-- power on/off/cycle targets::
+- add or create tunnels
 
-    $ tcf power-on [TARGETSPEC [TARGETSPEC [..]]
-    $ tcf power-off [TARGETSPEC [TARGETSPEC [..]]
-    $ tcf power-cycle [TARGETSPEC [TARGETSPEC [..]]
+    $ tcf tunnel-add TARGETSPEC PORT [PROTOCOL] [IP-ADDR]
+    $ tcf tunnel-rm TARGETSPEC PORT [PROTOCOL] [IP-ADDR]
 
-- get power state::
+- list tunnels::
 
-    $ tcf power-get [TARGETSPEC [TARGETSPEC [..]]
-    $ tcf power-ls [TARGETSPEC [TARGETSPEC [..]]
+    $ tcf tunnel-ls [TARGETSPEC [TARGETSPEC [..]]
 
 
 """
 
 import argparse
-import json
 import logging
-import re
-import sys
 
 import commonl
+import tcfl.tc
 import tcfl.ui_cli
 
 logger = logging.getLogger("ui_cli_tunnel")
@@ -42,11 +38,12 @@ def _cmdline_tunnel_add(cli_args: argparse.Namespace):
     def _tunnel_add(target, cli_args):
         server_port = target.tunnel.add(
             cli_args.port, cli_args.ip_addr, cli_args.protocol)
-        print("%s:%d" % (target.rtb.parsed_url.hostname, server_port))
+        print(f"{target.rtb.parsed_url.hostname}:{server_port}")
 
     return tcfl.ui_cli.run_fn_on_each_targetspec(
-        _tunnel_rm, cli_args, only_one = True,
-        iface = "power", extensions_only = [ 'power' ])
+        _tunnel_add, cli_args, only_one = True,
+        iface = "tunnel", extensions_only = [ "tunnel" ],
+        projections = [ "interconnects" ])
 
 
 
@@ -58,34 +55,38 @@ def _cmdline_tunnel_rm(cli_args: argparse.Namespace):
 
     return tcfl.ui_cli.run_fn_on_each_targetspec(
         _tunnel_rm, cli_args, only_one = True,
-        iface = "power", extensions_only = [ 'power' ])
+        iface = "tunnel", extensions_only = [ "tunnel" ],
+        projections = [ "interconnects" ])
 
 
 
-def _cmdline_tunnel_ls(_cli_args: argparse.Namespace):
+def _cmdline_tunnel_ls(cli_args: argparse.Namespace):
 
     def _tunnel_list_by_target(target, _cli_args):
         for local_port, data in  target.tunnel.list().items():
             if not isinstance(data, dict):
                 continue
-            print("%s %s:%s %s:%s" % (
-                data['protocol'],
-                target.rtb.parsed_url.hostname, local_port,
-                data['ip_addr'], data['port']
-            ))
+            try:
+                print(f"{data['protocol']}"
+                      f" {target.rtb.parsed_url.hostname}:{local_port}"
+                      f" {data['ip_addr']}:{data['port']}")
+            except KeyError:
+                pass	# ignore, bad data stored
 
     return tcfl.ui_cli.run_fn_on_each_targetspec(
         _tunnel_list_by_target, cli_args, only_one = True,
-        iface = "power", extensions_only = [ 'power' ])
+        iface = "tunnel", extensions_only = [ "tunnel" ],
+        projections = [ "interconnects" ])
 
 
 
 def cmdline_setup(argsp):
+
     ap = argsp.add_parser(
         f"tunnel-add{tcfl.ui_cli.commands_new_suffix}",
         help = "create an IP tunnel")
     tcfl.ui_cli.args_verbosity_add(ap)
-    tcfl.ui_cli.args_targetspec_add(ap)
+    tcfl.ui_cli.args_targetspec_add(ap, targetspec_n = 1)
     ap.add_argument("port", metavar = "PORT", action = "store", type = int,
                     help = "Port to tunnel to")
     ap.add_argument("protocol", metavar = "PROTOCOL", action = "store",
@@ -104,7 +105,7 @@ def cmdline_setup(argsp):
     commonl.argparser_add_aka(argsp, "tunnel-rm", "tunnel-remove")
     commonl.argparser_add_aka(argsp, "tunnel-rm", "tunnel-delete")
     tcfl.ui_cli.args_verbosity_add(ap)
-    tcfl.ui_cli.args_targetspec_add(ap)
+    tcfl.ui_cli.args_targetspec_add(ap, targetspec_n = 1)
     ap.add_argument("port", metavar = "PORT", action = "store", type = int,
                     help = "Port to tunnel to")
     ap.add_argument("protocol", metavar = "PROTOCOL", action = "store",
@@ -121,5 +122,5 @@ def cmdline_setup(argsp):
         f"tunnel-ls{tcfl.ui_cli.commands_new_suffix}",
         help = "List existing IP tunnels")
     tcfl.ui_cli.args_verbosity_add(ap)
-    tcfl.ui_cli.args_targetspec_add(ap)
+    tcfl.ui_cli.args_targetspec_add(ap, targetspec_n = 1)
     ap.set_defaults(func = _cmdline_tunnel_ls)
