177d783e968d37d08ff66dc19df0e67ba7a05ef1
Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
2023-05-07 14:48:57 -0700

    tcf/servers-discover: move to tcfl.ui_cli_servers; improve messaging
    Try to do a better job at reporting new servers found, it was quite
    hard to find before. Still not perfect.

--- tcfl/ui_cli_servers.py
+++ tcfl/ui_cli_servers.py
@@ -15,6 +15,11 @@ learn more about them):
 
     $ tcf servers
 
+- Discover more servers, by indicating more servers and querying known
+  servers for other servers they might know::
+
+    $ tcf servers-discover [HOSTNAME]
+
 - Flush/delete list of available servers::
 
     $ tcf servers-flush
@@ -141,6 +146,53 @@ def _cmdline_servers(cli_args: argparse.Namespace):
 
 
 
+def _cmdline_servers_discover(cli_args: argparse.Namespace):
+
+    log_sd = logging.getLogger("server-discovery")
+    tcfl.ui_cli.logger_verbosity_from_cli(log_sd, cli_args)
+
+    server_count0 = len(tcfl.server_c.servers)
+    log_sd.warning("at start I know about %d servers", server_count0)
+    if cli_args.flush:
+        log_sd.warning("flushing cached server list (--flush given)")
+        tcfl.server_c.flush()
+    else:
+        log_sd.warning("keeping cached server list (--flush not given)")
+    if log_sd.isEnabledFor(logging.DEBUG):
+        count = 0
+        for url, _server in tcfl.server_c.servers.items():
+            log_sd.debug("starting server[%d]: %s", count, url)
+            count += 1
+
+    tcfl.server_c.discover(
+        ssl_ignore = cli_args.ssl_ignore,
+        seed_url = cli_args.items,
+        seed_port = cli_args.port,
+        herds_exclude = cli_args.herd_exclude,
+        herds_include = cli_args.herd_include,
+        loops_max = cli_args.iterations,
+        max_cache_age = 0,	        # force discoverying all known servers
+        ignore_cache = cli_args.flush,
+        zero_strikes_max = cli_args.zero_strikes_max,
+        origin = "command line",
+    )
+    server_count = len(tcfl.server_c.servers)
+    server_delta = server_count - server_count0
+    if server_delta > 0:
+        log_sd.error("NOTICE: found %d new server/s"
+                     " (%d total)", server_delta, server_count)
+    elif len(cli_args.items) > 0:
+        # we already have some servers, but found no new ones and we
+        # weren't gvien any seeds;; might be an error
+        log_sd.error("no new servers (%d known) from command line seed: %s",
+                     server_count, ' '.join(cli_args.items))
+    else:
+        # were given no seed, suggest
+        log_sd.error("no new servers (%d known)."
+                     " You can try passing a server's name", server_count)
+
+
+
 def _cmdline_servers_flush(_args):
     tcfl.server_c.flush()
 
@@ -154,6 +206,43 @@ def cmdline_setup(arg_subparser):
     tcfl.ui_cli.args_verbosity_add(ap)
     tcfl.ui_cli.args_targetspec_add(ap)
     ap.set_defaults(func = _cmdline_servers)
+
+
+    ap = arg_subparser.add_parser(
+        "servers-discover",
+        help = "Discover servers")
+    tcfl.ui_cli.args_verbosity_add(ap)
+    ap.add_argument(
+        "--iterations", "-e", type = int, metavar = "MAX_LOOPS",
+        action = "store", default = 10,
+        help = "Maximum number of iterations [%(default)d]")
+    ap.add_argument(
+        "--zero-strikes-max", "-z", type = int, metavar = "MAX",
+        action = "store", default = 4,
+        help = "Stop after this many iterations [%(default)d] finding"
+        " no new servers")
+    ap.add_argument(
+        "--flush", "-f", action = 'store_true', default = False,
+        help = "Flush existing cached entries before")
+    ap.add_argument(
+        "--ssl-ignore", "-s", action = 'store_false', default = True,
+        help = "Default to ignore SSL validation [False]")
+    ap.add_argument(
+        "--port", "-p", action = 'store', type = int, default = 5000,
+        help = "Default port when none specified %(default)s")
+    ap.add_argument(
+        "--herd-exclude", "-x", metavar = "HERDNAME", action = 'append',
+        default = [], help = "Exclude herd (can be given multiple times)")
+    ap.add_argument(
+        "--herd-include", "-i", metavar = "HERDNAME", action = 'append',
+        default = [], help = "Include herd (can be given multiple times)")
+    ap.add_argument(
+        "items", metavar = "HOSTNAME|URL", nargs = "*",
+        action = "store", default = [],
+        help = "List of URLs or hostnames to seed discovery"
+        f" [{' '.join(tcfl.server_c._seed_default.keys())}]")
+    ap.set_defaults(func = _cmdline_servers_discover)
+
 
     ap = arg_subparser.add_parser(
         "servers-flush",
