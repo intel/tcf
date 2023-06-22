794d21899c5a47037760a29c11535802a546015b
Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
2023-06-21 21:27:23 -0700

    tcf/console-{enable,disable}: move to CLI UI framework for performance and consistency

--- tcfl/ui_cli_console.py
+++ tcfl/ui_cli_console.py
@@ -48,6 +48,30 @@ def _cmdline_console_ls(cli_args: argparse.Namespace):
 
 
 
+def _console_disable(target, console):
+    target.console.disable(console)
+
+def _cmdline_console_disable(cli_args: argparse.Namespace):
+
+    return tcfl.ui_cli.run_fn_on_each_targetspec(
+        _console_disable, cli_args, cli_args.console,
+        only_one = True,
+        iface = "console", extensions_only = [ "console" ])
+
+
+
+def _console_enable(target, console):
+    target.console.enable(console)
+
+def _cmdline_console_enable(cli_args: argparse.Namespace):
+
+    return tcfl.ui_cli.run_fn_on_each_targetspec(
+        _console_enable, cli_args, cli_args.console,
+        only_one = True,
+        iface = "console", extensions_only = [ "console" ])
+
+
+
 def _cmdline_setup(arg_subparser):
     ap = arg_subparser.add_parser(
         "console-ls",
@@ -55,3 +79,29 @@ def _cmdline_setup(arg_subparser):
     tcfl.ui_cli.args_verbosity_add(ap)
     tcfl.ui_cli.args_targetspec_add(ap, targetspec_n = 1)
     ap.set_defaults(func = _cmdline_console_ls)
+
+
+
+def _cmdline_setup_advanced(arg_subparser):
+
+    ap = arg_subparser.add_parser(
+        "console-disable",
+        help = "Disable a console")
+    tcfl.ui_cli.args_verbosity_add(ap)
+    tcfl.ui_cli.args_targetspec_add(ap, targetspec_n = 1)
+    ap.add_argument(
+        "-c", "--console", metavar = "CONSOLE",
+        action = "store", default = None,
+        help = "name of console to disable")
+    ap.set_defaults(func = _cmdline_console_disable)
+
+    ap = arg_subparser.add_parser(
+        "console-enable",
+        help = "Enable a console")
+    tcfl.ui_cli.args_verbosity_add(ap)
+    tcfl.ui_cli.args_targetspec_add(ap, targetspec_n = 1)
+    ap.add_argument(
+        "-c", "--console", metavar = "CONSOLE",
+        action = "store", default = None,
+        help = "name of console to enable")
+    ap.set_defaults(func = _cmdline_console_enable)
