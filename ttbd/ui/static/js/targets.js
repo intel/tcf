8d62fd52ccf24bc15eb379143f4f60107cc1b077
benjamin santana <benjamin.santana.velazquez@intel.com>
2024-01-15 21:05:36 +0000

    ui/static/js/targets.js: new js file 4 targets.js
    the user will be able to acquire systems from targets.html but wont be
    able to do much else, there is no sense in loading EVERY function in the
    main.js file.
    
    It is cleaner to just load a new file, targets.js, with just the
    function needed. And adding that file to the headers of both
    targets.html and target.html.
    
    Also created a mini css file for targets.html, with the same reasoning.
    With time we would have the css files in a modular fashion. Just loading
    the selectors each html file needs. This is the first step into that
    direction.
    
    Signed-off-by: benjamin santana <benjamin.santana.velazquez@intel.com>

---
+++ ttbd/ui/static/js/targets.js
@@ -0,0 +1,51 @@
+"use strict";
+
+/*
+* make an http request to make an allocation for the current user
+*
+* @param {target_list} list -> list of targets id  to acquire, (if you want to
+*                               only acquire one, then just make a list of 1
+*                               target id.)
+*
+* return {void} -> it reloads the windows though
+*/
+async function acquire(target_list) {
+
+    // we need a group id for the allocation, so we do a similar approach to
+    // the cli `tcf`, and we just join all the targets id in the reservation
+    // with a comma, creating a string like:
+    // target1,target2,target3
+    let group_id = target_list.join(',');
+
+    let r = await fetch('/ttb-v2/allocation', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+            'groups': {
+                group_id: target_list
+            },
+            'queue': false,
+            "endtime": 'static',
+      })
+    });
+
+    let b = await r.text();
+
+    if (r.status == 401) {
+        alert(
+            'oops, seems that you are not logged in. Please log in to' +
+            ' acquire machines (top right corner)'
+        );
+        return
+    }
+
+    if (r.status != 200) {
+        alert(b);
+        return
+    }
+
+    window.location.reload()
+}
+
