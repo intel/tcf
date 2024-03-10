c993d46659295eaed479a8044edf1654b7faf4b7
Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
2024-03-09 17:11:37 -0800

    commonl.buildah_image_create(): add ability to map extra directories
    We'll use this to map DNF caches so when it takes too long to download
    and the process timestout, a retry will just pick it up from where it
    timedout before.
    
    This happens first time we build the image: ttbd service starts, takes
    too long to download it all, systemd kills ttbd, restarts and keeps
    rebuilding without having to download from the get go.
    
    Signed-off-by: Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>

--- commonl/__init__.py
+++ commonl/__init__.py
@@ -3550,35 +3550,68 @@ def rpyc_compress_dnload_file(remote, remote_name, local_name = None):
                                 local_name)
 
 def buildah_image_create(image_name, dockerfile_s, maybe = True,
-                         timeout = 20, capture_output = True):
-    """
-    Build a container image using buildah
+                         timeout = 20, capture_output = True,
+                         extra_volume_maps: dict = None):
+    """Build a container image using buildah
 
-    :returns bool: *True* if the image was built, *False* if not
-      because it already existed
+    :param dict extra_volume_maps: (optional) dictionary of volumes to
+      map to build the container image with *-v* in the format::
+
+        { SOURCEPATH: DESTPATH[:MAPFLAGS] }
+
+      This is usually done to pass volumes in the host to the
+      container, so that every DESTPATH will be showing in the
+      container, but actually giving access to SOURCEPATH in the host.
+
+      For MAPFLAGS meaning, see *buildah* and *podman* documentation
+      for the *-v* command line option.
+
+      WARNING!! This might give write access from the container to the
+      host, use carefully.
+
+      Example to add DNF cache shared amongs the build instances:
+
+      >>> { "/var/dnf/cache": "/var/tmp/ttbd.dnf.cache" }
+
+    :returns tuple(bool, subprocess.CompletedProcess): If bool is
+      *True* if the image was built, *False* if not because it already
+      existed.  If built, also return the
+      :class:`subprocess.CompletedProcess` which contains return arg
+      info and output.
 
     FIXME: add --annotation cfg_hash so we can always refresh them
     based on the config file -> if it changes
     """
+    if extra_volume_maps == None:
+        extra_volume_maps = {}
+    else:
+        assert_dict_of_strings(extra_volume_maps, "volume")
     if maybe:
         # since this can take a while, if we see it already exists, we
         # don't redo it
-        p = subprocess.run([ "buildah", "images", "--format", "{{.Name}}" ],
-                           capture_output = True, check = True, timeout = 5,
-                           text = 'utf-8')
+        p = subprocess.run(
+            [ "buildah", "images", "--format", "{{.Name}}" ],
+            capture_output = True, check = True, timeout = 5,
+            text = 'utf-8')
         if image_name in p.stdout:
-            return False
+            return ( False, None )
 
     with tempfile.NamedTemporaryFile() as f:
         # see ttbl.power.daemon_c
         f.write(dockerfile_s.encode('utf-8'))
         f.flush()
-        subprocess.run(
-            [
-                "buildah", "bud", "-f", f.name, "-t", image_name,
-            ], check = True, capture_output = capture_output, text = 'utf-8',
+        cmdline = [ "buildah", "bud", "--pull=true" ]
+        for k, v in extra_volume_maps.items():
+            cmdline += [ "-v", f"{k}:{v}" ]
+        cmdline += [ "-f", f.name, "-t", image_name ]
+        p = subprocess.run(
+            cmdline,
+            check = True,
+            capture_output = capture_output,
+            text = 'utf-8',
             timeout = timeout)
-        return True
+        return ( True, p )
+
 
 
 class fsdb_c(object):
