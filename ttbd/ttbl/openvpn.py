254b60ec0ac7d7e361e0999299d09e81b066ed25
Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
2026-04-28 09:28:24 -0700

    ttbl.openvpn:  add TCP/UDP support, TAP suffixing, and improve server setup/docs
    - Add ip_protocol (udp|tcp) and tap_suffix parameters with
      validation, can run parallel instances
    
    - Use explicit TAP device names (<bridge_ifname>.<suffix>) instead of
      generic tap (to run parallel)
    
    - Export OpenVPN protocol/port as target properties

--- ttbd/ttbl/openvpn.py
+++ ttbd/ttbl/openvpn.py
@@ -15,61 +15,191 @@ import ttbl.power
 
 class server_c(ttbl.power.daemon_c):
 
-    openvpn_path = "/usr/bin/openvpn"
+    openvpn_path = "/usr/sbin/openvpn"
 
     """This class implements a power control that can be used to start an
     OpenVPN endpoint to access a private network.
+
+
+    :param str ip_protocol: (optional, default *udp*) the IP protocol
+      to use for OpenVPN; can be *udp* or *tcp*; UDP is more efficient
+      and reliable, TCP allows hopping firewalls.
+
+      Note the client needs to be configured to do one or the other
+
+    :param str tap_suffix: (*optional*, default *tap*)
+
+
 
     With a configuration such as::
 
       import ttbl.openvpn
 
-      ttbl.test_target.get('nwx').power.impl_add("openvpn", ttbl.openvpn.server_c())
+      nwx = ttbl.test_target.get('nwx')
+      # ... assumes the power and console interfaces are already made
+      # (e.g.) with target_vlan_add()
+      nwx.interface_impl_add("power", "netif", ttbl.vlan.netif_c())
+      nwx.property_set("interfaces.power.netif.vlan_taps", "tcp udp")
 
-    It creates an L2 bridge using tap the private network has a
+      nwx.interface_impl_add(
+          "power", "openvpn_udp",
+          ttbl.openvpn.server_c(openvpn_port = 3456,
+                                ip_protocol = "udp", tap_suffix = "udp"))
+      nwx.interface_impl_add(
+           "console", "log-openvpn-udp",
+           ttbl.console.logfile_c("openvpn-openvpn_udp.log"))
 
-    OpenVPN Client:
+      nwx.interface_impl_add(
+          "power", "openvpn_udp",
+          ttbl.openvpn.server_c(openvpn_port = 3456,
+                                ip_protocol = "tcp", tap_suffix = "tcp"))
+      nwx.interface_impl_add(
+           "console", "log-openvpn-tcp",
+           ttbl.console.logfile_c("openvpn-openvpn_tcp.log"))
 
-      $ tcf certs-get nwx -s openvpn
-      downloaded client certificate key -> nwx.openvpn.{key,cert}
-      $ tcf certs-get nwx -s ca
-      downloaded root-of-trust certificate -> nwx.ca.cert
-      $ /usr/bin/openvpn --client --dev tap --proto udp4 --remote SERVERIP 1194 \
-          --ca nwx.ca.cert --cert nwx.openvpn.cert --key nwx.openvpn.key \
-          --verb 4
+
+    It creates two L2 bridges using TAP, one over TCP, another over
+    UDP; then it adds two OpenVPN endpoints (over TCP and UDP) that
+    can be used to connect with an OpenVPN Client. Note it requires
+    the :class:`ttbl.vlan.netif_c` driver running before in the power
+    rail to create the network interfaces that are needed (a bridge
+    called *nwx*, an associated network interface called *nwx.tap*)
+
+    1. download the certificates for the network (every time the
+       allocation changes this needs to be done)::
+
+         $ tcf certs-get nwx -s ca openvpn
+         downloaded root-of-trust certificate -> nwx.ca.cert
+         downloaded client certificate key -> nwx.openvpn.{key,cert}
+
+    2. connect the VPN::
+
+         $ /usr/bin/openvpn --client --dev tap --proto udp4 --remote SERVER 3456 \
+            --ca nwx.ca.cert --cert nwx.openvpn.cert --key nwx.openvpn.key \
+            --verb 1
+         ...
+         2026-04-17 14:28:11 TUN/TAP device tap0 opened
+         2026-04-17 14:28:11 Initialization Sequence Completed
+
+
+    3. Get an IP address from the NUT's DHCP server for *tap0* (from
+       the *TUN/TAP device tap0 opened* message from OpenVPN.
+
+       - with systemd-networkd (recommended):
+
+         a. generate a configuration file::
+
+              $ sudo tee /run/systemd/network/90-tap0.network <<EOF
+              [Match]
+              Name=tap0
+
+              [Network]
+              DHCP=yes
+              EOF
+
+         b. tell system-network to reload and configure *tap0*::
+
+              $ sudo networkctl reload
+              $ sudo networkctl reconfigure tap0
+
+         c. verify configuration was obtained and DNS too::
+
+              $ networkctl status tap0
+              . 132: tap0
+                                 Link File: /usr/lib/systemd/network/99-default.link
+                              Network File: /run/systemd/network/90-tap0.network
+                                     State: routable (configured)
+                              Online state: online
+                                            ...
+                                      Type: ether
+                                      Kind: tun
+                                    Driver: tun
+                          Hardware Address: c2:5b:0e:31:c1:fa
+                                   Address: 192.2.213.56 (DHCPv4 via 192.2.80.1)
+                                            fe80::c05b:eff:fe31:c1fa
+                                       DNS: 192.2.80.1
+                                            ....
+
+              $ resolvectl status tap0
+              Link 132 (tap0)
+                  Current Scopes: DNS LLMNR/IPv4 LLMNR/IPv6
+                       Protocols: +DefaultRoute LLMNR=resolve -mDNS -DNSOverTLS DNSSEC=no/unsupported
+              Current DNS Server: 192.2.80.1
+                     DNS Servers: 192.2.80.1
+                   Default Route: yes
+
+       - with dhclient::
+
+           $ dhclient -i tap0
+
+         Note depending on what script integrations your version of
+         dhclient has (usually in */usr/bin/dhclient-script*), it
+         might alter the DNS configuration in ways that might affect
+         your system's configuration.
+
+       (the device might change)
+
     """
 
-    def __init__(self, openvpn_port: int = 1194, openvpn_path: str = None):
+    def __init__(self, openvpn_port: int = 1194, openvpn_path: str = None,
+                 ip_protocol: str = "udp", tap_suffix: str = "tap"):
+        assert ip_protocol in [ "udp", "tcp" ], \
+            f"ip_protocol: expected *udp* or *tcp*; got {ip_protocol}"
+        assert isinstance(openvpn_port, int) \
+            and openvpn_port > 2 and openvpn_port < 65536, \
+            f"openvpn_port: expected int 2-65536, got [{type(openvpn_port)}] '{openvpn_port}'"
+
         if openvpn_path == None:
             self.openvpn_path = self.openvpn_path
         else:
             self.openvpn_path = openvpn_path
         self.openvpn_port = openvpn_port
+        self.ip_protocol = ip_protocol
         cmdline = [
             self.openvpn_path,
             "--daemon", "openvpn-%(id)s-%(component)s",
             # so we capture it to the .stderr file we can stream over
             # console interface
-            #"--errors-to-stderr",
-            "--log", "/dev/stderr",
-            #"--log", "%(path)s/openvpn-%(component)s.log",
-            # in our convention, IC -- networks -- have the ipv4_addr as a top level record
-            "--local", "%(ipv4_addr)s",
+            "--log", "%(path)s/openvpn-%(component)s.log",
+            # Listen on all the network interfaces
+            # FIXME: how do do it so it doesn't listen on the NUTs --
+            # doesn't really matter, we need the certs
+            "--local", "0.0.0.0",
             # we need one port per NUT
             "--port", str(self.openvpn_port),
-            "--proto", "udp",	# UDP: more efficient and reliable
-            "--dev", "tap",	# tap: un dynamic tap device, so we can do DHCP
+            # UDP: more efficient and reliable
+            # TCP: allows hopping firewalls
+            "--proto", ip_protocol,
+            # we use a TAP device so we can do DHCP; the tap device is
+            # created by ttbl.conf_00_lib.vlan_setup; tap_ifname in
+            # target.kws is set by vlan_pci if we create a tap device
+            "--dev-type", "tap",
+            "--dev", f"%(bridge_ifname)s.{tap_suffix}",
             "--server-bridge",	# bridge: so we can do DHCP and L2 routing
             "--ca", "%(path)s/certificates/ca.cert",
             "--cert", "%(path)s/certificates/server.cert",
             "--key", "%(path)s/certificates/server.key",
             "--dh", "%(path)s/certificates/dh.pem",
-            "--writepid", "%(path)s/openvpn-%(component)s.pid"
+            "--writepid", "%(path)s/openvpn-%(component)s.pid",
+            # otherwise log files get wild
+            # FIXME: get from inventory intefaces.power.component.openvpn_verbosity
+            "--verb", "2",
         ]
         ttbl.power.daemon_c.__init__(
             self, cmdline, precheck_wait = 0.5, mkpidfile = False,
             pidfile = "%(path)s/openvpn-%(component)s.pid")
-        self.upid_set("OpenVPN server")
+        self.upid_set(
+            f"OpenVPN server on {ip_protocol}#{openvpn_port} server",
+            ip_protocol = ip_protocol,
+            openvpn_port = openvpn_port)
+
+
+
+    def target_setup(self, target, iface_name, component):
+        target.property_set(f"interfaces.{iface_name}.{component}.openvpn_protocol",
+                            self.ip_protocol)
+        target.property_set(f"interfaces.{iface_name}.{component}.openvpn_port",
+                            self.openvpn_port)
 
 
 
